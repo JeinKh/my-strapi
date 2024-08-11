17a3111efa536b81132a00489cdba34292454ed6
JeinKh <juliakhezhun@gmail.com>
2024-08-11 20:56:30 +0300

    add code

--- job-board/src/index.js
+++ job-board/src/index.js
@@ -1,13 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Login from "./pages/Login";
+// import Test from "./pages/test";
+import Dashboard from "./pages/dashboard";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Apply from "./pages/apply";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="login" element={<Login />} />
+        {/* <Route path="test" element={<Test />} /> */}
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="apply" element={<Apply />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
